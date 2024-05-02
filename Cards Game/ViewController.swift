3427f395d5e751393caab2d719aaabf60411e00c
Gastronom <terlimbombom0@gmail.com>
2024-05-02 09:37:03 +0300

    Initial Commit

---
+++ Cards Game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cards Game
+//
+//  Created by Gastronom on 2.05.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
