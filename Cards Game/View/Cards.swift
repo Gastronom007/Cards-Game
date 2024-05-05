2c6569e008c71cada6411256d72e743a0a70a478
Gastronom <terlimbombom0@gmail.com>
2024-05-05 14:17:45 +0300

    Implemeted Cards object

---
+++ Cards Game/View/Cards.swift
@@ -0,0 +1,8 @@
+//
+//  Cards.swift
+//  Cards Game
+//
+//  Created by Gastronom on 5.05.24.
+//
+
+import Foundation
---
+++ Cards Game/View/Shapes.swift
@@ -0,0 +1,8 @@
+//
+//  Shapes.swift
+//  Cards Game
+//
+//  Created by Gastronom on 5.05.24.
+//
+
+import Foundation
